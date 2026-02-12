1d434a9c3db93ae5701c7cb825d194a77f14a0c6
Joao Soupa <joaopedrosoupa@gmail.com>
2026-02-12 00:13:51 -0800

    Criando os containers de infra que sao os PostgreSQL e RabbitMQ

---
+++ commander/commander/src/test/java/com/uptimekube/commander/CommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptimekube.commander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
